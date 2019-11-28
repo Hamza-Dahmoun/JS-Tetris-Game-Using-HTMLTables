f3bba98f8d84d4b689f586fa3f4efe1b1e874ae3
Hamza <hamza.dahmoun7@gmail.com>
2019-11-28 21:47:22 +0100

    remove tdClick() since it is useless

--- app.js
+++ app.js
@@ -29,22 +29,6 @@ const table =
     cell9: { leftBrotherId: "cell8", rightBrotherId: "cell10", brotherBeneathId: "cell16" }, cell10: { leftBrotherId: "cell9", rightBrotherId: "cell11", brotherBeneathId: "cell17" }, cell11: { leftBrotherId: "cell10", rightBrotherId: "cell12", brotherBeneathId: "cell18" }, cell12: { leftBrotherId: "cell11", rightBrotherId: "cell13", brotherBeneathId: "cell19" }, cell13: { leftBrotherId: "cell12", rightBrotherId: "cell14", brotherBeneathId: "cell20" }, cell14: { leftBrotherId: "cell13", rightBrotherId: "", brotherBeneathId: "cell21" }, cell15: { leftBrotherId: "", rightBrotherId: "cell16", brotherBeneathId: "cell22" }, cell16: { leftBrotherId: "cell15", rightBrotherId: "cell17", brotherBeneathId: "cell23" }, cell17: { leftBrotherId: "cell16", rightBrotherId: "cell18", brotherBeneathId: "cell24" }, cell18: { leftBrotherId: "cell17", rightBrotherId: "cell19", brotherBeneathId: "cell25" }, cell19: { leftBrotherId: "cell18", rightBrotherId: "cell20", brotherBeneathId: "cell26" }, cell20: { leftBrotherId: "cell19", rightBrotherId: "cell21", brotherBeneathId: "cell27" }, cell21: { leftBrotherId: "cell20", rightBrotherId: "", brotherBeneathId: "cell28" }, cell22: { leftBrotherId: "", rightBrotherId: "cell23", brotherBeneathId: "cell29" }, cell23: { leftBrotherId: "cell22", rightBrotherId: "cell24", brotherBeneathId: "cell30" }, cell24: { leftBrotherId: "cell23", rightBrotherId: "cell25", brotherBeneathId: "cell31" }, cell25: { leftBrotherId: "cell24", rightBrotherId: "cell26", brotherBeneathId: "cell32" }, cell26: { leftBrotherId: "cell25", rightBrotherId: "cell27", brotherBeneathId: "cell33" }, cell27: { leftBrotherId: "cell26", rightBrotherId: "cell28", brotherBeneathId: "cell34" }, cell28: { leftBrotherId: "cell27", rightBrotherId: "", brotherBeneathId: "cell35" }, cell29: { leftBrotherId: "", rightBrotherId: "cell30", brotherBeneathId: "cell36" }, cell30: { leftBrotherId: "cell29", rightBrotherId: "cell31", brotherBeneathId: "cell37" }, cell31: { leftBrotherId: "cell30", rightBrotherId: "cell32", brotherBeneathId: "cell38" }, cell32: { leftBrotherId: "cell31", rightBrotherId: "cell33", brotherBeneathId: "cell39" }, cell33: { leftBrotherId: "cell32", rightBrotherId: "cell34", brotherBeneathId: "cell40" }, cell34: { leftBrotherId: "cell33", rightBrotherId: "cell35", brotherBeneathId: "cell41" }, cell35: { leftBrotherId: "cell34", rightBrotherId: "", brotherBeneathId: "cell42" }, cell36: { leftBrotherId: "", rightBrotherId: "cell37", brotherBeneathId: "cell43" }, cell37: { leftBrotherId: "cell36", rightBrotherId: "cell38", brotherBeneathId: "cell44" }, cell38: { leftBrotherId: "cell37", rightBrotherId: "cell39", brotherBeneathId: "cell45" }, cell39: { leftBrotherId: "cell38", rightBrotherId: "cell40", brotherBeneathId: "cell46" }, cell40: { leftBrotherId: "cell39", rightBrotherId: "cell41", brotherBeneathId: "cell47" }, cell41: { leftBrotherId: "cell40", rightBrotherId: "cell42", brotherBeneathId: "cell48" }, cell42: { leftBrotherId: "cell41", rightBrotherId: "", brotherBeneathId: "cell49" }, cell43: { leftBrotherId: "", rightBrotherId: "cell44", brotherBeneathId: "cell50" }, cell44: { leftBrotherId: "cell43", rightBrotherId: "cell45", brotherBeneathId: "cell51" }, cell45: { leftBrotherId: "cell44", rightBrotherId: "cell46", brotherBeneathId: "cell52" }, cell46: { leftBrotherId: "cell45", rightBrotherId: "cell47", brotherBeneathId: "cell53" }, cell47: { leftBrotherId: "cell46", rightBrotherId: "cell48", brotherBeneathId: "cell54" }, cell48: { leftBrotherId: "cell47", rightBrotherId: "cell49", brotherBeneathId: "cell55" }, cell49: { leftBrotherId: "cell48", rightBrotherId: "", brotherBeneathId: "cell56" }, cell50: { leftBrotherId: "", rightBrotherId: "cell51", brotherBeneathId: "cell57" }, cell51: { leftBrotherId: "cell50", rightBrotherId: "cell52", brotherBeneathId: "cell58" }, cell52: { leftBrotherId: "cell51", rightBrotherId: "cell53", brotherBeneathId: "cell59" }, cell53: { leftBrotherId: "cell52", rightBrotherId: "cell54", brotherBeneathId: "cell60" }, cell54: { leftBrotherId: "cell53", rightBrotherId: "cell55", brotherBeneathId: "cell61" }, cell55: { leftBrotherId: "cell54", rightBrotherId: "cell56", brotherBeneathId: "cell62" }, cell56: { leftBrotherId: "cell55", rightBrotherId: "", brotherBeneathId: "cell63" }, cell57: { leftBrotherId: "", rightBrotherId: "cell58", brotherBeneathId: "cell64" }, cell58: { leftBrotherId: "cell57", rightBrotherId: "cell59", brotherBeneathId: "cell65" }, cell59: { leftBrotherId: "cell58", rightBrotherId: "cell60", brotherBeneathId: "cell66" }, cell60: { leftBrotherId: "cell59", rightBrotherId: "cell61", brotherBeneathId: "cell67" }, cell61: { leftBrotherId: "cell60", rightBrotherId: "cell62", brotherBeneathId: "cell68" }, cell62: { leftBrotherId: "cell61", rightBrotherId: "cell63", brotherBeneathId: "cell69" }, cell63: { leftBrotherId: "cell62", rightBrotherId: "", brotherBeneathId: "cell70" }, cell64: { leftBrotherId: "", rightBrotherId: "cell65", brotherBeneathId: "cell71" }, cell65: { leftBrotherId: "cell64", rightBrotherId: "cell66", brotherBeneathId: "cell72" }, cell66: { leftBrotherId: "cell65", rightBrotherId: "cell67", brotherBeneathId: "cell73" }, cell67: { leftBrotherId: "cell66", rightBrotherId: "cell68", brotherBeneathId: "cell74" }, cell68: { leftBrotherId: "cell67", rightBrotherId: "cell69", brotherBeneathId: "cell75" }, cell69: { leftBrotherId: "cell68", rightBrotherId: "cell70", brotherBeneathId: "cell76" }, cell70: { leftBrotherId: "cell69", rightBrotherId: "", brotherBeneathId: "cell77" }, cell71: { leftBrotherId: "", rightBrotherId: "cell72", brotherBeneathId: "cell78" }, cell72: { leftBrotherId: "cell71", rightBrotherId: "cell73", brotherBeneathId: "cell79" }, cell73: { leftBrotherId: "cell72", rightBrotherId: "cell74", brotherBeneathId: "cell80" }, cell74: { leftBrotherId: "cell73", rightBrotherId: "cell75", brotherBeneathId: "cell81" }, cell75: { leftBrotherId: "cell74", rightBrotherId: "cell76", brotherBeneathId: "cell82" }, cell76: { leftBrotherId: "cell75", rightBrotherId: "cell77", brotherBeneathId: "cell83" }, cell77: { leftBrotherId: "cell76", rightBrotherId: "", brotherBeneathId: "cell84" }, cell78: { leftBrotherId: "", rightBrotherId: "cell79", brotherBeneathId: "cell85" }, cell79: { leftBrotherId: "cell78", rightBrotherId: "cell80", brotherBeneathId: "cell86" }, cell80: { leftBrotherId: "cell79", rightBrotherId: "cell81", brotherBeneathId: "cell87" }, cell81: { leftBrotherId: "cell80", rightBrotherId: "cell82", brotherBeneathId: "cell88" }, cell82: { leftBrotherId: "cell81", rightBrotherId: "cell83", brotherBeneathId: "cell89" }, cell83: { leftBrotherId: "cell82", rightBrotherId: "cell84", brotherBeneathId: "cell90" }, cell84: { leftBrotherId: "cell83", rightBrotherId: "", brotherBeneathId: "cell91" },
 }
 
-//lets add click event just to check if 'table' is built correctly
-for (let i = 0; i < td.length; i++) {
-    td[i].addEventListener("click", tdClick);
-}
-function tdClick(event) {
-    //this function will colorize the brothers of the td clicked by the user
-    //so lets colorize (for 1 second) its concerned brothers who will receive his movements
-    document.getElementById(table[event.target.id].leftBrotherId).className = "cell-on";
-    document.getElementById(table[event.target.id].rightBrotherId).className = "cell-on";
-    document.getElementById(table[event.target.id].brotherBeneathId).className = "cell-on";
-    setTimeout(function () {
-        document.getElementById(table[event.target.id].leftBrotherId).className = "cell-off";
-        document.getElementById(table[event.target.id].rightBrotherId).className = "cell-off";
-        document.getElementById(table[event.target.id].brotherBeneathId).className = "cell-off";
-    }, 1000);
-}
 
 //********** Players Objects ***********//
 var currentPlayerObject;
@@ -92,9 +76,12 @@ function choosePlayerRandomly() {
 
 
 
-//choosePlayerRandomly();
+//Inside the following function will switch on a player and randomly choose a new player
+//we'll pass in a random number to represent a random player
 displayNextPlayer_asCurrentPlayer(Math.floor(Math.random() * 8));
-chooseNextPlayer();
+//chooseNextPlayer();
+//we'll move down the player every second, the player is passed in as a parameter
+//'currentPlayerObject' was assigned a random player in the function switchOnPlayer() 
 var timeInterval=1000;
 const myInterval= window.setInterval(function(){movePlayerDown(currentPlayerObject);},timeInterval);
 //setTimeout(function(){clearInterval(myInterval);},5*timeInterval);
@@ -161,8 +148,8 @@ function movePlayerDown(currentPlayerObject) {
         //lets stop the layer from moving down automatically every second
         clearInterval(myInterval);
         displayNextPlayer_asCurrentPlayer(nextPlayerNumber);
-        chooseNextPlayer();
-    }
+        //chooseNextPlayer();
+        }
 }
 function switchoffPlayerCells_andAssignNewCells(currentPlayerObject) {
     //this function switch off all the cells of the player, and assign new cells to it (cells beneath)
@@ -643,10 +630,8 @@ function displayNextPlayer_asCurrentPlayer(nextPlayerNumber) {
 
     //1- lets display nextPlayer as currentPlayer in the stadium table    
     switchOnPlayer(nextPlayerNumber);
-    //now lets start moving the player down every second
-    myInterval= window.setInterval(function(){movePlayerDown(currentPlayerObject);},timeInterval);
     //2- lets choose a new nextPlayer and display it in the second table
-    chooseNextPlayer();
+    chooseNextPlayer();    
 }
 
 //******************** END NEXT PLAYER DISPLAY ******************//
