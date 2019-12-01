da65f81a14cf20cd20f44cf0f1e52859426427cb
Hamza <hamza.dahmoun7@gmail.com>
2019-12-02 00:19:36 +0100

    fix bug in switchoffPlayerCells_andAssignNewCells_toTheLeft() cauzd by new prop 'type'

--- app.js
+++ app.js
@@ -389,7 +389,7 @@ function switchoffPlayerCells_andAssignNewCells_toTheLeft(currentPlayerObject) {
     let player_propertiesCount = Object.keys(currentPlayerObject).length;
 
     //2- Now lets proceed
-    for (let i = 1; i <= player_propertiesCount; i++) {
+    for (let i = 1; i < player_propertiesCount; i++) {
         //lets build the appropriate property name
         let idPropertyName_in_currentPlayer = "cell" + i + "_Id";
         //now lets get this cellID
